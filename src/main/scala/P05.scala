e46807e60081b827100c521d5d1b88331be45979
Sam Starling <sam@soundcloud.com>
2016-09-15 21:29:58 +0200

    Simplify solution to P05: list reversal

--- src/main/scala/P05.scala
+++ src/main/scala/P05.scala
@@ -1,8 +1,6 @@
 object P05 {
   def reverse[A](list: List[A]): List[A] = list match {
-    case Nil => list
-    case e :: Nil => list
     case e :: tail => reverse(tail) ::: List(e)
-    case _ => ???
+    case Nil => list
   }
 }
