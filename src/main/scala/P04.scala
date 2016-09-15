132c20fc796ee1b587c4020426e1a99ea3777f90
Sam Starling <sam@soundcloud.com>
2016-09-15 21:22:21 +0200

    Implement recursive solution to P04

--- src/main/scala/P04.scala
+++ src/main/scala/P04.scala
@@ -1,3 +1,9 @@
 object P04 {
-  def length[A](list: List[A]): Int = ???
+  def length[A](list: List[A]): Int = list match {
+    case Nil => 0
+    case _ :: Nil => 1
+    case _ :: tail => 1 + length(tail)
+  }
+
+  def lengthTailRec[A](list: List[A]): Int = ???
 }
--- src/test/scala/P04Spec.scala
+++ src/test/scala/P04Spec.scala
@@ -3,9 +3,15 @@ import org.scalatest._
 
 class P04Spec extends FlatSpec with Matchers {
 
-  val list: List[String] = List("a", "b", "c", "d", "e")
+  "length" should "return 0 for an empty list" in {
+    P04.length(List.empty) should be(0)
+  }
+
+  "length" should "return 1 for a single-item list" in {
+    P04.length(List("a")) should be(1)
+  }
 
   "length" should "return the length of a list" in {
-    P04.length(list) should be(5)
+    P04.length(List("a", "b", "c", "d")) should be(4)
   }
 }
