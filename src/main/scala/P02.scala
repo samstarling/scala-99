0aac6f4d047dd28fd7c48643937271eec6171084
Sam Starling <sam@soundcloud.com>
2016-09-15 21:01:15 +0200

    First attempt at penultimate

--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -1,3 +1,7 @@
 object P02 {
-  def penultimate[A](list: List[A]): A = ???
+  def penultimate[A](list: List[A]): A = list match {
+    case e :: _ :: Nil => e
+    case _ :: tail => penultimate(tail)
+    case _ => ???
+  }
 }
